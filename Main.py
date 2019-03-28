31afd911d6a17c95dcdd3acad2ffc23c9b2c1970
Eric <elymberopoulos@luc.edu>
2019-03-27 19:26:35 -0500

    Asteroid classes move to their own src directory

--- Main.py
+++ Main.py
@@ -1,56 +1,8 @@
-import pygame
-import pygame.event
-from src.ship import Ship
-import sys
-import random
-
-class Asteroid(pygame.sprite.Sprite):
-    def __init__(self, windowWidth, windowHeight):
-        self.windowWidth = windowWidth
-        self.windowHeight = windowHeight
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((20,20))
-        self.image.fill((150,150,150))
-        self.rect = self.image.get_rect()
-        #Set start location and speeds
-        self.rect.x = random.randrange(windowWidth - self.rect.width)
-        self.rect.y = random.randrange(-100, -50)
-        self.speed_y = random.randrange(1,4)
-        self.speed_x = random.randrange(-2,2)
-
-    def update(self):
-        self.rect.y += self.speed_y
-        self.rect.x += self.speed_x
-
-        if self.rect.top > self.windowHeight + 15 or self.rect.left < -15 or self.rect.right > self.windowWidth:
-            self.rect.x = random.randrange(self.windowWidth - self.rect.width)
-            self.rect.y = random.randrange(-100, -50)
-            self.speed_y = random.randrange(1,4)
-            self.speed_x = random.randrange(-2,2)
-
-class Asteroid2(pygame.sprite.Sprite):
-    def __init__(self, windowWidth, windowHeight):
-        self.windowWidth = windowWidth
-        self.windowHeight = windowHeight
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((20,20))
-        self.image.fill((150,150,150))
-        self.rect = self.image.get_rect()
-        #Set start location and speeds
-        self.rect.x = -20
-        self.rect.y = random.randrange(0, windowHeight-100)
-        self.speed_y = random.randrange(-2,2)
-        self.speed_x = random.randrange(1,3)
-
-    def update(self):
-        self.rect.y += self.speed_y
-        self.rect.x += self.speed_x
-
-        if self.rect.top > self.windowHeight + 15 or self.rect.left < -15 or self.rect.right > self.windowWidth:
-            self.rect.x = random.randrange(-20, -10)
-            self.rect.y = random.randrange(0, self.windowHeight-100)
-            self.speed_y = random.randrange(1,4)
-            self.speed_x = random.randrange(1,3)
+import pygame, pygame.event, sys
+from src.asteroid import Asteroid
+from src.asteroid import Asteroid2
+from src.asteroid import Asteroid3
+from src.player import Player
 
 def gameExit():
     pygame.quit()
@@ -78,14 +30,18 @@ def main():
     game_sprites = pygame.sprite.Group()
     asteroidsOnScreen = pygame.sprite.Group()
     for i in range(10):
-        if i % 2 == 0:
-            asteroid = Asteroid(windowWidth, windowHeight)
+        if i <= 2:
+            asteroid = Asteroid.Asteroid(windowWidth, windowHeight)
+            game_sprites.add(asteroid)
+            asteroidsOnScreen.add(asteroid)
+        elif i <= 6:
+            asteroid = Asteroid2.Asteroid2(windowWidth, windowHeight)
             game_sprites.add(asteroid)
             asteroidsOnScreen.add(asteroid)
         else:
-            asteroid = Asteroid2(windowWidth, windowHeight)
+            asteroid = Asteroid3.Asteroid3(windowWidth, windowHeight)
             game_sprites.add(asteroid)
-            asteroidsOnScreen.add(asteroid)
+            asteroidsOnScreen.add(asteroid) 
 
         
 
