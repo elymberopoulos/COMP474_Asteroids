a0f34afccf038790d4e3130d0a0643476f3a4d41
Eric <elymberopoulos@luc.edu>
2019-03-31 23:07:23 -0500

    Slight restructuring. Needs more refactoring of classes out of root directory

--- Main.py
+++ Main.py
@@ -1,84 +1,5 @@
-<<<<<<< HEAD
-import pygame, pygame.event, sys
-from src.asteroid import Asteroid
-from src.asteroid import Asteroid2
-from src.asteroid import Asteroid3
-from src.player import Player
-
-def gameExit():
-    pygame.quit()
-    sys.exit()
-
-def main():
-    #Setup basic variables and methods for pygame
-    pygame.init()
-    windowWidth = 800
-    windowHeight = 700
-    fps = 45
-    clock = pygame.time.Clock()
-    gameWindow = pygame.display.set_mode((windowWidth, windowHeight))
-    pygame.display.set_caption("Asteroids")
-    surface = pygame.Surface((50,50))
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-
-    #SHIP POSITION
-    shipX = windowWidth/2
-    shipY = windowWidth/2
-    shipSpeed = 4
-
-    #Variables to keep track of sprites on the screen
-    game_sprites = pygame.sprite.Group()
-    asteroidsOnScreen = pygame.sprite.Group()
-    for i in range(10):
-        if i <= 2:
-            asteroid = Asteroid.Asteroid(windowWidth, windowHeight)
-            game_sprites.add(asteroid)
-            asteroidsOnScreen.add(asteroid)
-        elif i <= 6:
-            asteroid = Asteroid2.Asteroid2(windowWidth, windowHeight)
-            game_sprites.add(asteroid)
-            asteroidsOnScreen.add(asteroid)
-        else:
-            asteroid = Asteroid3.Asteroid3(windowWidth, windowHeight)
-            game_sprites.add(asteroid)
-            asteroidsOnScreen.add(asteroid) 
-
-        
-
-    while(True):
-        
-        #Possible bounding box for initial ship
-        pygame.draw.ellipse(gameWindow, WHITE, (shipX, shipY, 20, 30))
-        game_sprites.draw(gameWindow)
-        game_sprites.update()
-        #Draw Triangle
-        # pygame.draw.lines(gameWindow, WHITE, True, ((400, 350), (450, 400), (350, 400), 1))
-        pygame.display.update()
-
-        #Monitor the FPS of the game
-        clock.tick(fps)
-
-        for event in pygame.event.get():
-            # ________________________________________
-            if event.type == pygame.QUIT:
-                gameExit()
-        
-        rotate = 0
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]: shipY -= shipSpeed
-        if pressed[pygame.K_DOWN]: shipY += shipSpeed
-        if pressed[pygame.K_LEFT]: shipX -= shipSpeed
-        if pressed[pygame.K_RIGHT]: shipX += shipSpeed
-        if pressed[ord('a')]: rotate = pygame.transform.rotate(surface, -20)
-        if pressed[ord('d')]: rotate = pygame.transform.rotate(surface, 20)
-
-        gameWindow.fill(BLACK)
-        # 'flip' display - always after drawing...
-        pygame.display.flip()
-=======
 import sys
-from Asteroid import *
+from src.asteroid import Asteroid, Asteroid2, Asteroid3
 from Player import Player
 from Constants import *
 
@@ -98,22 +19,17 @@ def game_exit():
     pygame.quit()
     sys.exit()
 
->>>>>>> Jay_Branch
-
 #Variables to keep track of sprites on the screen
 
 for i in range(10):
-    asteroid = Asteroid()
-    '''
+    if i <= 2:
+        Asteroid.Asteroid()
     elif i <= 6:
-        asteroid = Asteroid2.Asteroid2(windowWidth, windowHeight)
-        game_sprites.add(asteroid)
-        asteroidsOnScreen.add(asteroid)
+        Asteroid2.Asteroid2()
     else:
-        asteroid = Asteroid3.Asteroid3(windowWidth, windowHeight)
-        game_sprites.add(asteroid)
-        asteroidsOnScreen.add(asteroid)
-    '''
+        Asteroid3.Asteroid3()
+
+
 
 
 # main game loop
--- src/asteroid/Asteroid3.py
+++ src/asteroid/Asteroid3.py
@@ -1,29 +1,30 @@
 import pygame, random
-
+import Constants
 """
 This class of asteroids spawn at the right side of the screen and move to the left side of the window.
 """
 
 class Asteroid3(pygame.sprite.Sprite):
-    def __init__(self, windowWidth, windowHeight):
-        self.windowWidth = windowWidth
-        self.windowHeight = windowHeight
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((20,20))
         self.image.fill((150,150,150))
         self.rect = self.image.get_rect()
         #Set start location and speeds
-        self.rect.x = self.windowWidth + 20
-        self.rect.y = random.randrange(0, windowHeight-100)
+        self.rect.x = Constants.WIN_WIDTH + 20
+        self.rect.y = random.randrange(0, Constants.WIN_HEIGHT-100)
         self.speed_y = random.randrange(-2,2)
         self.speed_x = random.randrange(-3,-1)
+
+        Constants.GAME_SPRITES.add(self)
+        Constants.ASTEROIDS.add(self)
 
     def update(self):
         self.rect.y += self.speed_y
         self.rect.x += self.speed_x
 
-        if self.rect.top > self.windowHeight + 15 or self.rect.left < -15 or self.rect.right > self.windowWidth:
-            self.rect.x = self.windowWidth -30
-            self.rect.y = random.randrange(0, self.windowHeight-100)
+        if self.rect.top > Constants.WIN_HEIGHT + 15 or self.rect.left < -15 or self.rect.right > Constants.WIN_WIDTH:
+            self.rect.x = Constants.WIN_WIDTH -30
+            self.rect.y = random.randrange(0, Constants.WIN_HEIGHT-100)
             self.speed_y = random.randrange(-2,2)
             self.speed_x = random.randrange(-3,-1)
